1cf7dc3836d4645b8bcc8ec6343390a769be61b5
eyanez0711 <111064506+eyanez0711@users.noreply.github.com>
2022-09-05 01:57:06 -0400

    Update Cylinder2.java

--- Cylinder2.java
+++ Cylinder2.java
@@ -1,3 +1,9 @@
-class public Cylinde2r{
+class public Cylinder2{
 public static void main(String [] args){
-  int
+  int radius=2;
+  int Pi=3.1416;
+  int height=6;
+  int Base_Area= (radius*Pi)^2;
+  int volume= Pi*radius^2 *height;
+  }
+  }
