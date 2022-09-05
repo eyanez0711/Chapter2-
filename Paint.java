17658c512d8fd96e5196c31e042473771f8271fc
eyanez0711 <111064506+eyanez0711@users.noreply.github.com>
2022-09-05 02:09:16 -0400

    Create Paint.java

---
+++ Paint.java
@@ -0,0 +1,15 @@
+class public Paint{
+  public statoc void main(Static [] args){
+    int length=20;
+    int width=20;
+    int height=8;
+    int gallon=350;
+    int window=15;
+    int door=21;
+  int number_windows=3;
+    int walls=(length*height)*2+(width*height)*2;
+    int area= walls-(window*number_windows)-door;
+      int gal_need=area/gallon;
+      int quart= gal_need/4;
+  }
+}
