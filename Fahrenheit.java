40f11bf0fef3fc89118b0040be8e75f2cc1c652f
eyanez0711 <111064506+eyanez0711@users.noreply.github.com>
2022-09-05 01:20:32 -0400

    Update Fahrenheit.java

--- Fahrenheit.java
+++ Fahrenheit.java
@@ -1,1 +1,9 @@
-public class Fahrenheit
+public class Fahrenheit{
+  public static void main(Static[] args){
+    final double F_PER_C=33.8;
+    double Fahrenheit=100.0;
+    
+    double Celsius=(Fahrenheit-32)*5/9;
+    System.out.println(Fahrenheit + " fahrenheit= " + Celsius+ "Celsius");
+  }
+}
