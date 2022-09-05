fd967508e4f9c9161d160248f52fccba04853c99
eyanez0711 <111064506+eyanez0711@users.noreply.github.com>
2022-09-05 02:17:18 -0400

    Create Weight.java

---
+++ Weight.java
@@ -0,0 +1,10 @@
+class public weight{
+  public static void main(String [] args){
+    int pound=180;
+    int Kilo= pound/2.2;
+      int height_ft=5;
+      int height_inch=9;
+      int meters= height_inch+(height_ft/12)/0.0254;
+        int BMI= kilo/(meters)^2;
+  }
+}
